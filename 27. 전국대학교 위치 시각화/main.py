dc02db6c9a6ee8cec8da42b31e52c868a7fa3302
covy-gyu <kdjin11@naver.com>
2023-11-08 01:44:37 +0900

    [Add] 27. 학교 위치 시각화

---
+++ 27. 전국대학교 위치 시각화/main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import requests
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import re
+
+# https://kess.kedi.re.kr/index
+# https://www.vworld.kr/dev/v4dv_geocoderguide2_s001.do
+
+filePath = r"27. 전국대학교 위치 시각화\학교주소좌표.xlsx"
+df_from_excel = pd.read_excel(filePath, engine="openpyxl")
+
+df_from_excel.columns = df_from_excel.loc[4].tolist()
+
+df_from_excel = df_from_excel.drop(index=list(range(0, 5)))
+
+print(df_from_excel.head())
+print(df_from_excel["학교명"].values)
+print(df_from_excel["주소"].values)
+
+apiurl = "https://api.vworld.kr/req/address?"
+params = "service=address&request=getCoord&version=2.0&crs=epsg:4326&refine=true&simple=-false&format=json&type="
+road_type = "ROAD"
+road_type2 = "PARCEL"
+address = "&address="
+keys = "&key="
+primary_key = "EA54B124-084B-3BF4-866B-5B0485B5A2D2"
+
+
+def request_geo(road):
+    page = requests.get(
+        apiurl + params + road_type + address + road + keys + primary_key
+    )
+    json_data = page.json()
+    if json_data["response"]["status"] == "OK":
+        x = json_data["response"]["result"]["point"]["x"]
+        y = json_data["response"]["result"]["point"]["y"]
+        return x, y
+    else:
+        x = 0
+        y = 0
+        return x, y
+
+
+try:
+    wb = load_workbook(r"27. 전국대학교 위치 시각화\학교주소좌표.xlsx", data_only=True)
+    sheet = wb.active
+except:
+    wb = Workbook()
+    sheet = wb.active
+
+university_list = df_from_excel["학교명"].to_list()
+address_list = df_from_excel["주소"].to_list()
+
+for num, value in enumerate(address_list):
+    addr = re.sub(r"\([^)]*\)", "", value)
+    print(addr)
+    x, y = request_geo(addr)
+    sheet.append([university_list[num], addr, x, y])
+
+wb.save(r"27. 전국대학교 위치 시각화\학교주소좌표.xlsx")
